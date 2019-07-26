f352e34289f281b08bd6ff614bfdca962a1d2c0d
Reishou <reishou90@gmail.com>
2019-07-26 16:08:55 +0700

    register enum provider

--- src/Si6BaseServiceProvider.php
+++ src/Si6BaseServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Si6\Base;
 
+use BenSampo\Enum\EnumServiceProvider;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\ServiceProvider;
 use Si6\Base\Exceptions\Handler;
@@ -14,6 +15,7 @@ class Si6BaseServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(ExceptionHandler::class, Handler::class);
+        $this->app->register(EnumServiceProvider::class);
         $this->app->middleware([
             Unsupported::class,
             Unacceptable::class,
