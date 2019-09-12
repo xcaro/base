d985843ac7386f5c28d291536a8ad4ea82494340
Reishou <reishou90@gmail.com>
2019-09-12 14:12:40 +0700

    refactor (structure): add global middleware

--- src/Providers/Si6BaseServiceProvider.php
+++ src/Providers/Si6BaseServiceProvider.php
@@ -7,11 +7,15 @@ use GuzzleHttp\ClientInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 use Si6\Base\Exceptions\Handler;
 use Si6\Base\Http\Middleware\Authenticate;
 use Si6\Base\Http\Middleware\BeforeResponse;
+use Si6\Base\Http\Middleware\CheckForMaintenanceMode;
+use Si6\Base\Http\Middleware\TrimStrings;
+use Si6\Base\Http\Middleware\TrustProxies;
 use Si6\Base\Http\Middleware\Unacceptable;
 use Si6\Base\Http\Middleware\Unsupported;
 use Si6\Base\Http\Middleware\Versioning;
@@ -25,16 +29,38 @@ class Si6BaseServiceProvider extends ServiceProvider
     {
         $this->app->singleton(ExceptionHandler::class, Handler::class);
 
+        $this->registerGlobalMiddleware();
+        $this->registerMicroservicesDependence();
+        $this->registerAuthProvider();
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    protected function registerGlobalMiddleware()
+    {
         $kernel = $this->app->make(Kernel::class);
+
+        $kernel->prependMiddleware(TrimStrings::class);
+        $kernel->prependMiddleware(CheckForMaintenanceMode::class);
+        $kernel->prependMiddleware(TrustProxies::class);
+        $kernel->prependMiddleware(ConvertEmptyStringsToNull::class);
+
         $kernel->prependMiddleware(Unsupported::class);
         $kernel->prependMiddleware(Unacceptable::class);
         $kernel->prependMiddleware(BeforeResponse::class);
+    }
 
+    protected function registerMicroservicesDependence()
+    {
         $this->mergeConfigFrom(__DIR__ . '/../../config/microservices.php', 'microservices');
         $this->app->bind(ClientInterface::class, function ($app, $options) {
             return new Client($options);
         });
+    }
 
+    protected function registerAuthProvider()
+    {
         $this->mergeConfigFrom(__DIR__ . '/../../config/auth.php', 'auth');
         $this->app->register(AuthServiceProvider::class);
     }
