6bc9b1ca04e5b98a3ebf08bc80d2220e7dd73ad0
Reishou <reishou90@gmail.com>
2019-09-18 01:07:25 +0700

    chore: set upper message

--- src/Exceptions/VersionInvalid.php
+++ src/Exceptions/VersionInvalid.php
@@ -2,16 +2,18 @@
 
 namespace Si6\Base\Exceptions;
 
+use Illuminate\Support\Str;
+
 class VersionInvalid extends BaseException
 {
     protected $statusCode = 400;
 
     public function __construct(string $version = "")
     {
-        $message = "We don't support api version $version";
+        $message = "WE_DO_NOT_SUPPORT_VERSION_" . Str::upper($version);
 
         if (!$version) {
-            $message = 'API version is required';
+            $message = 'API_VERSION_IS_REQUIRED';
         }
 
         parent::__construct($message);
